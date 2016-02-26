a7b73bcc508514ddea372942d8dd515b44a7cbc6
Dez Ertim <TurgayErtim@Turgays-MBP.home>
2016-02-26 03:10:32 +0000

    One motor

---
+++ Accelerometer/Accelerometer.ino
@@ -0,0 +1,16 @@
+const unsigned X_AXIS_PIN = 2;
+const unsigned Y_AXIS_PIN = 1;
+const unsigned Z_AXIS_PIN = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.print(analogRead(X_AXIS_PIN));
+  Serial.print(" ");
+  Serial.print(analogRead(Y_AXIS_PIN));
+  Serial.print(" ");
+  Serial.println(analogRead(Z_AXIS_PIN));
+  delay(100);
+}
